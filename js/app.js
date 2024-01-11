ebbeb1865464c68a24696d3e43a5dc31c6668273
XasanovBoburjon0922 <xasanovboburr0922@gmail.com>
2024-01-11 19:13:11 +0500

    menu-edit

--- js/app.js
+++ js/app.js
@@ -10,11 +10,11 @@ elModeBtn.addEventListener("click", function(){
 elHamburgerBtn.addEventListener("click", function() {
     elModal.classList.toggle("show-modal");
     elHamburgerBtn.classList.toggle("remove-self")
-    elHamburgerClose.classList.toggle("open")
+    elHamburgerClose.classList.toggle("open-close")
 })
 
 elHamburgerClose.addEventListener("click", function() {
     elModal.classList.toggle("show-modal");
     elHamburgerBtn.classList.toggle("remove-self")
-    elHamburgerClose.classList.toggle("close")
+    elHamburgerClose.classList.toggle("open-close")
 })
