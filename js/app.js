9c0a284c1a703699b4222dded0d1b4aa529e31cd
XasanovBoburjon0922 <xasanovboburr0922@gmail.com>
2024-01-11 14:14:22 +0500

    Product

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
 let elBody = document.querySelector("body")
 let elModeBtn = document.querySelector("#modeBtn") 
-
+let elHamburgerBtn = document.querySelector(".hamburger-btn")
+let elHamburgerClose = document.querySelector(".close")
+let elModal = document.querySelector(".model")
 
 elModeBtn.addEventListener("click", function(){
     elBody.classList.toggle("mode")
+})
+elHamburgerBtn.addEventListener("click", function() {
+    elModal.classList.toggle("show-modal");
+    elHamburgerBtn.classList.toggle("remove-self")
+    elHamburgerClose.classList.toggle("open-close")
+})
+
+elHamburgerClose.addEventListener("click", function() {
+    elModal.classList.toggle("show-modal");
+    elHamburgerBtn.classList.toggle("remove-self")
+    elHamburgerClose.classList.toggle("open-close")
 })
